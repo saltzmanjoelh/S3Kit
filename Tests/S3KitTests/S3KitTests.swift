d86f0f07c9f27eb96ee393b83c9c6076d7f0cc50
saltzmanjoelh <talk@joelsaltzman.com>
2016-12-17 01:12:06 -0800

    Added failing test

--- Tests/S3KitTests/S3KitTests.swift
+++ Tests/S3KitTests/S3KitTests.swift
@@ -9,6 +9,10 @@ class S3KitTests: XCTestCase {
     let path = "/tmp/\(UUID())"
     let credentialsPath = "/Users/joelsaltzman/Sites/S3Kit/s3Credentials.csv"
     
+    func testXcodeServerEmail(){
+        XCTFail()
+    }
+    
     func testUpload() {
 
         do{
