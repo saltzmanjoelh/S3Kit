1eec3764c0ce1ae929381197b09b910d190151da
Joel Saltzman <talk@joelsaltzman.com>
2018-03-26 00:16:44 -0700

    Added static lib type

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "S3Kit",
+            type: .static,
             targets: ["S3Kit"]),
     ],
     dependencies: [
